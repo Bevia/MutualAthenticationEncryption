692f25c3a36874545016f61c392d5d56e2e4d485
vbevia <vbevia@gmail.com>
2024-10-17 16:14:57 +0200

    add changes to git

--- app/src/main/java/com/bevia/mutualathenticationencryption/MainActivity.kt
+++ app/src/main/java/com/bevia/mutualathenticationencryption/MainActivity.kt
@@ -21,6 +21,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        testingRSAKeyGenAndStorage()
+        generateECCKeyPairAndStore()
+        signRSAPublicKeyWithECCPrivateKey("mistis", "ecc_keys")
     }
 
     private fun testingRSAKeyGenAndStorage() {
