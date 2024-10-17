14cf0ef75c769f827488c9c1a1d2ca087c570be1
vbevia <vbevia@gmail.com>
2024-10-17 17:12:08 +0200

    decompose with SOLID principles

--- app/src/main/java/com/bevia/encryption/ECCSigner.kt
+++ app/src/main/java/com/bevia/encryption/ECCSigner.kt
@@ -1,4 +1,7 @@
 package com.bevia.encryption
 
-class ECCSigner {
+class ECCSigner(private val keyManager: KeyGenerator) : Signer {
+    override fun signData(alias: String, data: ByteArray): String? {
+        // Signing logic here using the keyManager to get the private key
+    }
 }
--- app/src/main/java/com/bevia/encryption/ECCVerifier.kt
+++ app/src/main/java/com/bevia/encryption/ECCVerifier.kt
@@ -1,4 +1,7 @@
 package com.bevia.encryption
 
-class ECCVerifier {
+class ECCVerifier(private val keyManager: KeyGenerator) : Verifier {
+    override fun verifySignature(alias: String, data: ByteArray, signature: String): Boolean {
+        // Verification logic here using the keyManager to get the public key
+    }
 }
