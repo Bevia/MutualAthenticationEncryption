0b412b081e06537d754435e7c81ec6a7e1d0a375
vbevia <vbevia@gmail.com>
2024-10-23 10:21:51 +0200

    add canonicalize class

---
+++ app/src/main/java/com/bevia/mutualathenticationencryption/JSONcanonicalize.kt
@@ -0,0 +1,22 @@
+package com.bevia.mutualathenticationencryption
+
+/*
+how would I canonicalize this in kotlin?
+
+ var epoch = Date.now() / 1000;
+              var request = {
+                  "epoch": epoch.toString(),
+                  "method": "post",
+                  "query": "",
+                  "path": "/v1/users",
+                  "body": {
+                      "device_id": demo["device_id"],
+                      "public_key": public
+                  }
+              };
+
+ */
+class JSONcanonicalize {
+
+
+}
--- app/src/main/java/com/bevia/mutualathenticationencryption/MainActivity.kt
+++ app/src/main/java/com/bevia/mutualathenticationencryption/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.bevia.mutualathenticationencryption
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,7 +15,12 @@ import com.bevia.encryption.ecc.ECCKeyManager
 import com.bevia.encryption.ecc.ECCSigner
 import com.bevia.encryption.ecc.ECCVerifier
 import com.bevia.encryption.rsa.KeyStoreManagerImpl
+import com.bevia.encryption.rsa.PublicKeyOperations
 import com.bevia.encryption.rsa.RSAEncryptor
+import java.security.MessageDigest
+import java.util.UUID
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,10 +48,53 @@ class MainActivity : AppCompatActivity() {
 
         setupWindowInsets()
 
-        performCryptographicOperations()
+        getUUID(this)
 
-        // Generate API Key for RSA key for authentication
-        ApiKey().generateApiKey(getString(R.string.rsa_alias))
+        // Set the button listener to perform cryptographic operations
+        val cryptographyButton: Button = findViewById(R.id.cryptographyButton)
+        cryptographyButton.setOnClickListener {
+            performCryptographicOperations()
+
+            // Generate API Key for RSA key for authentication
+            ApiKey().generateApiKey(getString(R.string.rsa_alias))
+
+            Log.d("Mistis MainActivity", "Generated fingerprint:  " + generateDeviceFingerprint())
+        }
+    }
+
+    private fun generateDeviceFingerprint(): String {
+        val deviceInfo = "Brand:${Build.BRAND}, Model:${Build.MODEL}, " +
+                "Manufacturer:${Build.MANUFACTURER}, Version:${Build.VERSION.SDK_INT}, " +
+                "Hardware:${Build.HARDWARE}"
+
+        // Hash the device info to create a fingerprint
+        val digest = MessageDigest.getInstance("SHA-256")
+        val hashBytes = digest.digest(deviceInfo.toByteArray())
+        return hashBytes.joinToString("") { "%02x".format(it) }
+    }
+
+    private fun getUUID(context: Context): String {
+        val sharedPreferences: SharedPreferences =
+            context.getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+        var uuid = sharedPreferences.getString("UUID", null)
+
+        if (uuid == null) {
+            uuid = UUID.randomUUID().toString()
+            sharedPreferences.edit().putString("UUID", uuid).apply()
+        }
+
+        Log.d("Mistis UUID", "getUUID: $uuid")
+
+        return uuid
+    }
+
+
+    fun generateHmac(data: String, secret: String): String {
+        val secretKeySpec = SecretKeySpec(secret.toByteArray(), "HmacSHA256")
+        val mac = Mac.getInstance("HmacSHA256")
+        mac.init(secretKeySpec)
+        val hmacBytes = mac.doFinal(data.toByteArray())
+        return hmacBytes.joinToString("") { "%02x".format(it) }
     }
 
     private fun performCryptographicOperations() {
@@ -49,6 +102,8 @@ class MainActivity : AppCompatActivity() {
         cryptographyManager.handleRSAKeyGenAndStorage(getString(R.string.rsa_alias))
         cryptographyManager.generateECCKeyPair(getString(R.string.ecc_alias))
         cryptographyManager.signRSAPublicKeyWithECCPrivateKey(getString(R.string.rsa_alias), getString(R.string.ecc_alias))
+
+        PublicKeyOperations().fetchPublicKey(getString(R.string.rsa_alias))
     }
 
     private fun setupWindowInsets() {
