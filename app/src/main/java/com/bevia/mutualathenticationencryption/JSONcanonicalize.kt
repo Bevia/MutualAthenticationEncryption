e518bf0ba6f2a99173f387d9a81987b7cd6c3cb1
vbevia <vbevia@gmail.com>
2024-10-23 10:24:02 +0200

    add canonicalize class

--- app/src/main/java/com/bevia/mutualathenticationencryption/JSONcanonicalize.kt
+++ app/src/main/java/com/bevia/mutualathenticationencryption/JSONcanonicalize.kt
@@ -1,5 +1,8 @@
 package com.bevia.mutualathenticationencryption
 
+import org.json.JSONObject
+import java.util.TreeMap
+
 /*
 how would I canonicalize this in kotlin?
 
@@ -18,5 +21,50 @@ how would I canonicalize this in kotlin?
  */
 class JSONcanonicalize {
 
+    fun setCanonicalize(jsonObject: JSONObject): String {
+
+        val epoch = System.currentTimeMillis() / 1000
+
+        // Create the JSON object with the original structure
+        val request = JSONObject(
+            mapOf(
+                "epoch" to epoch.toString(),
+                "method" to "post",
+                "query" to "",
+                "path" to "/v1/users",
+                "body" to mapOf(
+                    "device_id" to "demo_device_id",  // Replace with actual device ID
+                    "public_key" to "your_public_key"  // Replace with actual public key
+                )
+            )
+        )
+
+        // Canonicalize the JSON object
+        val canonicalJson = canonicalizeJson(request)
+
+        // Print the canonicalized JSON string
+        println(canonicalJson)
+
+        return canonicalJson
+    }
+
+    fun canonicalizeJson(jsonObject: JSONObject): String {
+        // Sort the keys of the JSON object using TreeMap (which sorts keys by natural order)
+        val sortedMap = TreeMap<String, Any?>()
+
+        // Iterate through the keys and values of the JSONObject
+        for (key in jsonObject.keys()) {
+            val value = jsonObject.get(key)
+            // If the value is a nested JSONObject, recursively sort it as well
+            sortedMap[key] = if (value is JSONObject) {
+                canonicalizeJson(value) // Recursively sort nested objects
+            } else {
+                value
+            }
+        }
+
+        // Recreate a sorted JSON object from the sorted map
+        return JSONObject(sortedMap as Map<String, Any?>).toString()
+    }
 
 }
